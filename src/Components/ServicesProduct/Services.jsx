2ecdd33b1a668ba164781edee0766fc33bca7bc8
Matias Aramayo <matiasaramayo@hotmail.com>
2022-12-13 13:10:11 -0300

    servicios

--- src/Components/ServicesProduct/Services.jsx
+++ src/Components/ServicesProduct/Services.jsx
@@ -18,30 +18,24 @@ const Services = () => {
             <img src={kraft} alt="bolsas kraft" height={72} />
             <p className={styles.titleProducts}>Bolsas de papel Kraft</p>
             <p className={styles.descriptionProducts}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-              corporis vero cupiditate tempore eos modi illum odit impedit,
-              minima ea hic et velit inventore, nihil blanditiis eligendi?
-              Adipisci, soluta fugit.
+              Trabajo con diseño de bolsas de papel Kraft hace mas de 10 años.
+              Puedo realizar diseños personalizados a medida del cliente.
             </p>
           </Col>
           <Col lg={4} className={styles.products}>
             <img src={totebags} alt="totebags" height={72} />
             <p className={styles.titleProducts}>Totebags de lienzo</p>
             <p className={styles.descriptionProducts}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-              corporis vero cupiditate tempore eos modi illum odit impedit,
-              minima ea hic et velit inventore, nihil blanditiis eligendi?
-              Adipisci, soluta fugit.
+              Trabajo con diseño de Totebags de lienzo hace mas de 10 años.
+              Puedo realizar diseños personalizados a medida del cliente.
             </p>
           </Col>
           <Col lg={4} className={styles.productsFinal}>
             <img src={caja} alt="cajas estuche" height={72} />
             <p className={styles.titleProducts}>Cajas estuche con estampa</p>
             <p className={styles.descriptionProducts}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-              corporis vero cupiditate tempore eos modi illum odit impedit,
-              minima ea hic et velit inventore, nihil blanditiis eligendi?
-              Adipisci, soluta fugit.
+              Trabajo con diseño de estuche con estampa hace mas de 10 años.
+              Puedo realizar diseños personalizados a medida del cliente.
             </p>
           </Col>
         </Row>
